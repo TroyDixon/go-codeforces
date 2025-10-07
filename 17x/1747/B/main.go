d1df69462184be3abaeef891e5b7a46b86423d22
Troy Dixon <troydixon0608@gmail.com>
2025-10-07 08:50:26 -0500

    1747b

---
+++ 17x/1747/B/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var in, out = bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
+
+func solve() {
+	n := readInt()
+	i, j := 1, 3*n
+	fmt.Println(n/2 + n%2)
+	for i < j {
+		fmt.Println(i, j)
+		i += 3
+		j -= 3
+	}
+}
+
+func main() {
+	defer out.Flush()
+	var t int
+	for fmt.Fscanln(in, &t); t > 0; t-- {
+		solve()
+	}
+}
+
+func readInt() int {
+	var x int
+	fmt.Fscan(in, &x)
+	return x
+}
+
+func readStr() string {
+	var x string
+	fmt.Fscan(in, &x)
+	return x
+}
